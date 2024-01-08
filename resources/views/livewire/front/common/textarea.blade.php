225c49298aae4209b6bca2e400604c3dea340539
vijay-choudhary-sws <vijay98sws@gmail.com>
2024-01-08 17:52:00 +0530

    front design

--- resources/views/livewire/front/common/textarea.blade.php
+++ resources/views/livewire/front/common/textarea.blade.php
@@ -1,10 +1,13 @@
 <div>
-  
-    <div class="row my-3">
-        <div class="col-12">
-            <label for="{{$in_data->inputType->tag_name}}{{$in_data->id}}" class="mb-2 float-left">{{$in_data->label}}</label>
-           
-            <textarea wire:model="typetextarea" name="{{$in_data->name }}" id="{{$in_data->inputType->tag_name}}{{$in_data->id}}" class="form-control" placeholder="{{$in_data->placeholder}}"    ></textarea>
+
+    <div class="row">
+        <div class="col-12 mb-2">
+            <label for="{{$in_data->inputType->tag_name}}{{$in_data->id}}"
+                class="mb-2 float-left">{{$in_data->label}}</label>
+
+            <textarea wire:model="typetextarea" name="{{$in_data->name }}"
+                id="{{$in_data->inputType->tag_name}}{{$in_data->id}}" class="form-control"
+                placeholder="{{$in_data->placeholder}}"></textarea>
         </div>
     </div>
 </div>
--- resources/views/livewire/front/payment-detail.blade.php
+++ resources/views/livewire/front/payment-detail.blade.php
@@ -26,60 +26,59 @@
                                                 <div class="pb-3 rounded">
                                                     <div class="p-2">
                                                         <div class="row align-items-center">
-                                                            <div class="col-12">
-                                                                <label for="name"
-                                                                    class="mb-2 float-left">Name</label>
+                                                            <div class="col-12 mb-2">
+                                                                <label for="name" class="mb-2 float-left">Name</label>
                                                                 <input wire:model="name" type="text"
                                                                     class="form-control" id="name"
                                                                     placeholder="Enter your full name">
                                                             </div>
-                                                            <div class="col-12">
-                                                                <label for="email"
-                                                                    class="mb-2 float-left">Email</label>
+                                                            <div class="col-12  mb-2">
+                                                                <label for="email" class="mb-2 float-left">Email</label>
                                                                 <input wire:model="email" type="email"
                                                                     class="form-control" id="email"
                                                                     placeholder="Enter your email">
                                                             </div>
-                                                            <div class="col-12">
-                                                                <label for="phone"
-                                                                    class="mb-2 float-left">phone</label>
+                                                            <div class="col-12  mb-2">
+                                                                <label for="phone" class="mb-2 float-left">phone</label>
                                                                 <input wire:model="phone" type="number"
                                                                     class="form-control" id="phone"
                                                                     placeholder="Enter your number">
                                                             </div>
                                                             @foreach ($input_data as $d)
+
+                                                            
                                                             @if ($d->input_select_data)
-                                                            <livewire:is :component="'front.common.ExistingSelect'" livewire:common.
-                                                                :in_data="$d" :is_front="$front"
-                                                                :wire:key="$d->id"
-                                                                wire:model="formdata.{{ $d->id }}" />
-                                                        @else
-                                                            <livewire:is :component="'front.common.' .
-                                                                $d->inputType->tag_name" livewire:common.
-                                                                :in_data="$d" :is_front="$front"
-                                                                :wire:key="$d->id"
-                                                                wire:model="formdata.{{ $d->id }}" />
-                                                        @endif
+                                                            <livewire:is :component="'front.common.ExistingSelect'"
+                                                                livewire:common. :in_data="$d" :is_front="$front"
+                                                                :wire:key="$d->id" wire:model="formdata.{{ $d->id }}"
+                                                               />
+                                                            @else
+                                                            <livewire:is @saved="likeUpdated" :component="'front.common.' .
+                                                                $d->inputType->tag_name" livewire:common. :in_data="$d"
+                                                                :is_front="$front" :wire:key="$d->id"
+                                                                wire:model="formdata.{{ $d->id }}"   />
+                                                            @endif
                                                             @endforeach
 
-                                                            <div class="col-md-12 mt-3" id="amountChangelable">
+                                                            <div class="col-md-12 mb-2" id="amountChangelable">
 
                                                                 <div class="form-group">
                                                                     <label class="form-label">Amount</label>
-                                                                    <div class="input-group mb-3">
+                                                                    <div class="input-group ">
                                                                         <div class="input-group-prepend">
                                                                             <span class="input-group-text"
                                                                                 id="basic-addon1">
                                                                                 @if ($geolocation == 'IN')
-                                                                                    INR
+                                                                                INR
                                                                                 @else
-                                                                                    $
+                                                                                $
                                                                                 @endif
                                                                             </span>
                                                                         </div>
                                                                         <input type="number" class="form-control"
-                                                                            id="amount" wire:model="amount"
-                                                                            value="0" autocomplete="off"  @if($amountType == 1) disabled @endif>
+                                                                            id="amount" wire:model="amount" value="0"
+                                                                            autocomplete="off" @if($amountType==1)
+                                                                            disabled @endif>
                                                                     </div>
                                                                 </div>
                                                             </div>
